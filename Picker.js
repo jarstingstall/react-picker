6125b0502216b4c74400f48aac735094365383d8
jarstingstall <jrarst01@gmail.com>
2015-06-19 09:41:06 -0400

    add PickerList component

--- Picker.js
+++ Picker.js
@@ -1,12 +1,18 @@
 var React = require('react');
 var PickerSelect = require('./PickerSelect');
+var PickerList = require('./PickerList');
 
 require('./picker.scss');
 
 var Picker = React.createClass({
     render: function() {
         return (
-            <PickerSelect items={this.props.items} />
+            <div>
+                <PickerSelect items={this.props.items} />
+                <div className="picker-base">
+                    <PickerList items={this.props.items} />
+                </div>
+            </div>
         );
     }
 });
